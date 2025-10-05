bdd607eb0ff17f3f5e17e0c881c9fc651a9163ad
MohiAssaf <mohamedabuassaf@gmail.com>
2025-10-05 10:20:36 +0300

    setting the tailwind content on index html also for the smooth scroll to work

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "index.html"],
   theme: {
     extend: {
       colors: {
-        navy: '#0a192f',
-        'light-navy': '#112240',
-        'lightest-navy': '#233554',
-        slate: '#8892b0',
-        'light-slate': '#a8b2d1',
-        green: '#64ffda',
+        navy: "#0a192f",
+        "light-navy": "#112240",
+        "lightest-navy": "#233554",
+        slate: "#8892b0",
+        "light-slate": "#a8b2d1",
+        green: "#64ffda",
       },
       fontFamily: {
-        sans: ['Inter', 'sans-serif'],
-        mono: ['SF Mono', 'Fira Code', 'monospace'],
+        sans: ["Inter", "sans-serif"],
+        mono: ["SF Mono", "Fira Code", "monospace"],
       },
     },
   },
